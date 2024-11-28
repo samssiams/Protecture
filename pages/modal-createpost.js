512bc7b3c0bcfab669af9ce4c70d3037964e1fa3
samuelcruz-kyna <sam.cruz.kyna@gmail.com>
2024-11-28 20:12:05 +0800

    profanity .3

--- pages/modal-createpost.js
+++ pages/modal-createpost.js
@@ -31,7 +31,7 @@ export default function CreatePostModal({ isOpen, onClose, userData }) {
     "dick", "cunt", "piss", "crap", "slut", "whore", "prick", "fag", 
     "nigger", "motherfucker", "cock", "pussy", "retard", "douche", 
     "bullshit", "arsehole", "wanker", "tosser", "bloody", "bugger",
-    "fvck", "fck", "fcking", "mf" , "dfq" , "dick" , "pussy" , 
+    "fvck", "fck", "fcking", "mf" , "dfq" , "dick" , "pussy" , "MotherFucker" ,
   
     // Tagalog Profanity
     "putangina", "gago", "tanga", "bobo", "ulol", "lintik", "hinayupak", 
@@ -40,7 +40,7 @@ export default function CreatePostModal({ isOpen, onClose, userData }) {
     "gunggong", "salot", "walanghiya", "ampota", "syet", "gago", 
     "putcha", "punyemas", "hudas", "diyablo", "g@go", "8080", "kingina", "kupal",
     "t4nga", "b0b0", "inutil", "pakyu", "shet", "t4nga", "obob", "bob0",
-    "kinangina" 
+    "kinangina" , "tangina" , "hayuf" , "hayf" 
   ];
   
 
