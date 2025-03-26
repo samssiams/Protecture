830798460990ac470104bfd9af318adca1b602cd
Quinchy <deguzmancyriljames@gmail.com>
2025-03-26 11:44:06 +0800

    fix post count in home

--- pages/api/post/getposts.js
+++ pages/api/post/getposts.js
@@ -22,7 +22,6 @@ export default async function handler(req, res) {
         where: { 
           user_id: userId,
           archived: false,
-          communityPosts: { none: {} }
         },
       });
       return res.status(200).json({ count: postCount });
